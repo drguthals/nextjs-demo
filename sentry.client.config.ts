1bdd9bcdac5cb7b855444e504a8b179a3d122507
Sarah Guthals <sarah@guthals.com>
2025-02-04 15:37:02 -0800

    userfeedback

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -10,10 +10,7 @@ Sentry.init({
   // Add optional integrations for additional features
   integrations: [
     Sentry.replayIntegration(),
-    Sentry.feedbackIntegration({
-      // Additional SDK configuration goes in here, for example:
-      colorScheme: "system",
-    }),
+    Sentry.feedbackIntegration(),
     Sentry.featureFlagsIntegration(),
   ],
 
